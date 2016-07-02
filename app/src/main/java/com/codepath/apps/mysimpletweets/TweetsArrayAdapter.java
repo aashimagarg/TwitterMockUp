46b99a6b3170e50bbd396ea6c07bf08f8477b063
Aashima Garg <aashimagarg@fb.com>
2016-07-01 19:00:13 -0700

    BUTTERKNIFE AND BREAD WOOHOO

--- app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +23,8 @@ import org.w3c.dom.Text;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cz.msebera.android.httpclient.Header;
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
@@ -40,71 +43,87 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
         super(context, android.R.layout.simple_list_item_1, tweets);
     }
 
+    static class ViewHolder{
+        @BindView(R.id.ivImage) ImageView ivProfileImage;
+        @BindView(R.id.tvUserName) TextView tvUserName;
+        @BindView(R.id.tvFullName) TextView tvName;
+        @BindView(R.id.tvBody) TextView tvBody;
+        @BindView(R.id.tvTimeStamp) TextView tvTime;
+        @BindView(R.id.tvRTCount) TextView tvRTCount;
+        @BindView(R.id.tvLikeCount) TextView tvLikeCount;
+        @BindView(R.id.btnReply) Button btnReply;
+        @BindView(R.id.ivMedia) ImageView ivMedia;
+        @BindView(R.id.btnRetweet) Button btnRetweet;
+        @BindView(R.id.btnLike) Button btnLike;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
     //Override and setup custom template
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        final ViewHolder viewHolder;
         //1. Get the tweet
         final Tweet tweet = getItem(position);
         //2. Find/inflate the template
-        if (convertView == null){
+
+        if (convertView != null) {
+            viewHolder = (ViewHolder) convertView.getTag();
+        } else {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
         }
+
+
+
         //3. Find the subviews to fill with data in the template
-        final ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivImage);
-        TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
-        TextView tvName = (TextView) convertView.findViewById(R.id.tvFullName);
-        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
-        TextView tvTime = (TextView) convertView.findViewById(R.id.tvTimeStamp);
-        final TextView tvRTCount = (TextView) convertView.findViewById(R.id.tvRTCount);
-        final TextView tvLikeCount = (TextView) convertView.findViewById(R.id.tvLikeCount);
-        Button btnReply = (Button) convertView.findViewById(R.id.btnReply);
-        ImageView ivMedia = (ImageView) convertView.findViewById(R.id.ivMedia);
-        final Button btnRetweet = (Button) convertView.findViewById(R.id.btnRetweet);
-        final Button btnLike = (Button) convertView.findViewById(R.id.btnLike);
 
         //4. populate data into subviews
         Typeface tf = Typeface.createFromAsset(getContext().getAssets(),"fonts/GothamNBold.otf");
         Typeface tf2 = Typeface.createFromAsset(getContext().getAssets(),"fonts/GothamNBook.otf");
-        tvTime.setText(tweet.getCreatedAt());
-        tvTime.setTypeface(tf2);
-        tvUserName.setText(tweet.getUser().getScreenName());
-        tvUserName.setTypeface(tf2);
-        tvName.setText(tweet.getUser().getName());
-        tvName.setTypeface(tf2);
-        tvBody.setText(tweet.getBody());
-        tvBody.setTypeface(tf2);
-        tvRTCount.setText(String.valueOf(tweet.getRtCount()));
-        tvLikeCount.setText(String.valueOf(tweet.getLikeCount()));
+        viewHolder.tvTime.setText(tweet.getCreatedAt());
+        viewHolder.tvTime.setTypeface(tf2);
+        viewHolder.tvUserName.setText(tweet.getUser().getScreenName());
+        viewHolder.tvUserName.setTypeface(tf2);
+        viewHolder.tvName.setText(tweet.getUser().getName());
+        viewHolder.tvName.setTypeface(tf2);
+        viewHolder.tvBody.setText(tweet.getBody());
+        viewHolder.tvBody.setTypeface(tf2);
+        viewHolder.tvRTCount.setText(String.valueOf(tweet.getRtCount()));
+        viewHolder.tvLikeCount.setText(String.valueOf(tweet.getLikeCount()));
 
-        ivProfileImage.setImageResource(android.R.color.transparent);
-        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).transform(new RoundedCornersTransformation(3, 3)).into(ivProfileImage);
+        viewHolder.ivProfileImage.setImageResource(android.R.color.transparent);
+        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).transform(new RoundedCornersTransformation(3, 3)).into(viewHolder.ivProfileImage);
 
         //showing images of tweets - very hacky way.
         if (tweet.getMedia() != null){
-            ivMedia.setVisibility(View.VISIBLE);
-            Picasso.with(getContext()).load(tweet.getMedia()).transform(new RoundedCornersTransformation(3, 3)).into(ivMedia);
+            viewHolder.ivMedia.setVisibility(View.VISIBLE);
+            Picasso.with(getContext()).load(tweet.getMedia()).transform(new RoundedCornersTransformation(3, 3)).into(viewHolder.ivMedia);
         } else {
-            ivMedia.setVisibility(View.GONE);
+            viewHolder.ivMedia.setVisibility(View.GONE);
         }
 
         if (tweet.isRetweeted()){
-            btnRetweet.setBackgroundResource(R.drawable.retweet_action_on);
+            viewHolder.btnRetweet.setBackgroundResource(R.drawable.retweet_action_on);
         } else {
-            btnRetweet.setBackgroundResource(R.drawable.retweet_action);
+            viewHolder.btnRetweet.setBackgroundResource(R.drawable.retweet_action);
         }
         if (tweet.isFavorited()){
-            btnLike.setBackgroundResource(R.drawable.like_action_on);
+            viewHolder.btnLike.setBackgroundResource(R.drawable.like_action_on);
         } else {
-            btnLike.setBackgroundResource(R.drawable.like_action);
+            viewHolder.btnLike.setBackgroundResource(R.drawable.like_action);
         }
 
         //set on image click listener
-        ivProfileImage.setOnClickListener(new View.OnClickListener() {
+        viewHolder.ivProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (ivProfileImage != null) {
+                if (viewHolder.ivProfileImage != null) {
                     // Fire an intent when a photo is selected
                     Intent i = new Intent(v.getContext(), ProfileActivity.class);
                     // Pass contact object in the bundle and populate details activity.
@@ -115,7 +134,7 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
         });
 
         //set reply click listener
-        btnReply.setOnClickListener(new View.OnClickListener() {
+        viewHolder.btnReply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -130,7 +149,7 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
 
 
         //set retweet click listener
-        btnRetweet.setOnClickListener(new View.OnClickListener() {
+        viewHolder.btnRetweet.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick (View v) {
 
@@ -139,9 +158,9 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
                             @Override
                             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                                 super.onSuccess(statusCode, headers, response);
-                                btnRetweet.setBackgroundResource(R.drawable.retweet_action_on);
+                                viewHolder.btnRetweet.setBackgroundResource(R.drawable.retweet_action_on);
                                 tweet.rtCount++;
-                                tvRTCount.setText(String.valueOf(tweet.getRtCount()));
+                                viewHolder.tvRTCount.setText(String.valueOf(tweet.getRtCount()));
                                 tweet.isRetweeted = true;
                                 //try to figure out how to make tweet appear on profile view without a refresh (in real time)
                             }
@@ -156,9 +175,9 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
                             @Override
                             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                                 super.onSuccess(statusCode, headers, response);
-                                btnRetweet.setBackgroundResource(R.drawable.retweet_action);
+                                viewHolder.btnRetweet.setBackgroundResource(R.drawable.retweet_action);
                                 tweet.rtCount--;
-                                tvRTCount.setText(String.valueOf(tweet.getRtCount()));
+                                viewHolder.tvRTCount.setText(String.valueOf(tweet.getRtCount()));
                                 tweet.isRetweeted = false;
                                 //try to figure out how to make tweet appear on profile view without a refresh (in real time)
                             }
@@ -174,7 +193,7 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
         });
 
         //set favorite click listener
-        btnLike.setOnClickListener(new View.OnClickListener() {
+        viewHolder.btnLike.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
 
@@ -183,9 +202,9 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
                         @Override
                         public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                             super.onSuccess(statusCode, headers, response);
-                            btnLike.setBackgroundResource(R.drawable.like_action_on);
+                            viewHolder.btnLike.setBackgroundResource(R.drawable.like_action_on);
                             tweet.likeCount++;
-                            tvLikeCount.setText(String.valueOf(tweet.getLikeCount()));
+                            viewHolder.tvLikeCount.setText(String.valueOf(tweet.getLikeCount()));
                             tweet.isFavorited = true;
                             //try to figure out how to make tweet appear on profile view without a refresh (in real time)
                         }
@@ -200,9 +219,9 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
                         @Override
                         public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                             super.onSuccess(statusCode, headers, response);
-                            btnLike.setBackgroundResource(R.drawable.like_action);
+                            viewHolder.btnLike.setBackgroundResource(R.drawable.like_action);
                             tweet.likeCount--;
-                            tvLikeCount.setText(String.valueOf(tweet.getLikeCount()));
+                            viewHolder.tvLikeCount.setText(String.valueOf(tweet.getLikeCount()));
                             tweet.isFavorited = false;
                             //try to figure out how to make tweet appear on profile view without a refresh (in real time)
                         }
